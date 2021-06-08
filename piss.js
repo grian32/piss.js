0216d6e5e9aca38de3685d1998ec4ca9dd24b667
grian <54550982+grian32@users.noreply.github.com>
2021-06-08 18:26:16 +0300

    update jsdoc

--- piss.js
+++ piss.js
@@ -1,5 +1,5 @@
 /* Any elements that match the selector given have their background color changed to yellow and their text color to orange.
- * @param {string} selector - The selector.
+ * @param {string} selector The selector.
  */
 function piss(selector) {
     for (let i of document.querySelectorAll(selector)) {
